bc0b5cf22f47c6e78494f32d716ecf935c8896ca
Dallin Craven <dallincraven@gmail.com>
2025-08-18 12:20:55 -0600

    update database handling and folder structure

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from hike_logger import create_app
+from hike_logger import create_app, db
+from hike_logger import models
 
 app = create_app()
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-    
--- hike_logger/__init__.py
+++ hike_logger/__init__.py
@@ -1,13 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY']='dev'
-    app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite://hike_logger.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        'sqlite:///' + os.path.join(app.instance_path, 'hike_logger.db')
+        )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+     # Create instance folder if it doesn't exist
+    try:
+        os.makedirs(app.instance_path, exist_ok=True)
+    except OSError:
+        pass
+
+    db.init_app(app)
 
     from . import routes
     app.register_blueprint(routes.bp)
